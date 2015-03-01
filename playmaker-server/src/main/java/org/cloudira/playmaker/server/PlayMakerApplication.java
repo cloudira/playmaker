41fd49a0655b1c257fb00670b77db8eeb340e9d4
ztomic <zeljko.tomic@gmail.com>
2015-03-01 10:50:03 +0100

    Playmaker Server - Update for latest Spring Cloud Config Server snapshot.

--- playmaker-server/src/main/java/org/cloudira/playmaker/server/PlayMakerApplication.java
+++ playmaker-server/src/main/java/org/cloudira/playmaker/server/PlayMakerApplication.java
@@ -3,8 +3,8 @@ package org.cloudira.playmaker.server;
 import org.cloudira.playmaker.server.repository.ServiceRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.configure.server.EnableConfigServer;
-import org.springframework.cloud.configure.server.EnvironmentRepository;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.EnvironmentRepository;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
